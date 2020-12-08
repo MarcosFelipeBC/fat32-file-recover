def4ed65eb149831dcd2c552ade07c6d4fb9cbc5
MarcosFelipeBC <marcosfellipecosta@gmail.com>
2020-12-08 13:03:51 -0300

    Removed commented lines

--- main.c
+++ main.c
@@ -13,14 +13,8 @@ int main (){
     
     read(usb_file, fat_volume_id, 512);
 
-    // for (int i=0; i<512; i++) {
-    //     printf("%02X", fat_volume_id[i]);
-    //     if((i+1)%16 == 0) printf("\n");
-    //     else printf(" ");
-    // }
-
     int bytes_per_sector = fat_volume_id[0x0B] + (fat_volume_id[0x0C] * 256);
-	int sectors_per_cluster = fat_volume_id[0x0D];
+    int sectors_per_cluster = fat_volume_id[0x0D];
 	int reserved_sectors = fat_volume_id[0x0E] + (fat_volume_id[0x0F] * 256);
 	int num_of_fats = fat_volume_id[0x10];
 	int sectors_per_fat = *((unsigned int *)(fat_volume_id+0x24));
